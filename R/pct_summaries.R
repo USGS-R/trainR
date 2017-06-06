760234ee787ac124b5ad04d1381fb2416dd4fbeb
Reneau <pcreneau@usgs.gov>
2017-06-06 14:06:16 -0500

    added title description and param

--- R/pct_summaries.R
+++ R/pct_summaries.R
@@ -1,3 +1,10 @@
+#' @title Censored Data
+#' @description Flags data censored.
+#' @param  vals numeric amount of bad censored
+#' @examples
+#' 
+#' 
+#' 
 pct_censored <- function(vals, digits=2){
   cen.i <- which_censored(vals)
   cen.pct <- calc_pct(ninterest = length(cen.i),
@@ -6,6 +13,12 @@ pct_censored <- function(vals, digits=2){
   return(cen.pct)
 }
 
+#' @title Missing Data
+#' @description  Flags missing data
+#' @param vals  numeric amount of missing data
+#' @examples
+#' censored <- pct_missing()
+#'  
 pct_missing <- function(vals, digits=2){
   na.i <- which(is.na(vals))
   na.pct <- calc_pct(ninterest = length(na.i),
@@ -13,7 +26,12 @@ pct_missing <- function(vals, digits=2){
                      digits = digits)
   return(na.pct)
 }
-
+#' @title Percent of missing or bad data
+#' @param ninterest numeric amount of bad data
+#' @param ntotal  numeric total amount of data
+#' @examples 
+#' 
+#' 
 calc_pct <- function(ninterest, ntotal, digits=2){
   pct <- ninterest/ntotal*100
   pct.rnd <- round(pct)
