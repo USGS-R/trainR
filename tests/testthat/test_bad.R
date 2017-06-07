8c5a6c88bca4278ae2da389cc4589b2851116837
Reneau <pcreneau@usgs.gov>
2017-06-07 16:25:34 -0500

    added tests for pct_summaries

---
+++ tests/testthat/test_bad.R
@@ -0,0 +1,25 @@
+context("Missing Values")
+test_that("is pct_missing return correct value TRUE",{
+  expect_equal(pct_missing(c(1,1,1,NA)) , 25)
+})
+test_that("is pct_censored return correct value TRUE",{
+  expect_equal(pct_censored(c("<1",1,1,1)), 25)
+})
+test_that("is pct_missing return all NA TRUE",{
+  expect_equal(pct_missing(c(NA)),100)
+})
+test_that("is pct_censored return all censored TRUE",{
+  expect_equal(pct_censored(c("<1")),100)
+})
+test_that("is pct_missing return all data good TRUE",{
+  expect_equal(pct_missing(c(1,1,1,1)),0)
+})
+test_that("is pct_censored return all data good TRUE",{
+  expect_equal(pct_censored(c(1,1,1)),0)
+})
+test_that("is pct_missing return bad input TRUE",{
+  expect_warning(pct_missing(NULL)) 
+})
+#test_that("invalid pH returns FALSE",{
+#  expect_false(is_valid_pH(22))
+#})
